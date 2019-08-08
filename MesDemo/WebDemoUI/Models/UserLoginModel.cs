9f214da53f9f687eefb25eacccf0139a10ed6155
luwei <2073414181@qq.com>
2019-08-08 09:40:46 +0800

    system.web.dll引用

--- MesDemo/WebDemoUI/Models/UserLoginModel.cs
+++ MesDemo/WebDemoUI/Models/UserLoginModel.cs
@@ -1,11 +1,28 @@
 using System;
-using System.Web;
-using System.Web.Services;
+using Microsoft.AspNetCore.Http;
+using System.Web.Security;
+using System.Web.Script.Serialization;
 
-namespace MesDemo.WebUI.admin.ashx
+namespace MesDemo.WebUI.admin
 {
-    public class UserLogin  
+    public class UserLogin
     {
+        public void ProcessRequest(HttpContext Context)
+        {
+            Context.Request.ContentType = "application/json";
+            string action = Context.Request.Query["action"];
 
+            try
+            {
+                switch (action)
+                {
+                    case "getuser":
+                        if (Context.Request.IsHttps)
+                        {
+
+                        }
+                }
+            }
+        }
     }
 }
