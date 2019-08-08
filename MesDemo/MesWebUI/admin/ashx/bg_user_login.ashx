c74ef849efd52ed5097535c5072d3a5e9558139c
luwei <2073414181@qq.com>
2019-08-09 03:43:32 +0800

    login页面完善

---
+++ MesDemo/MesWebUI/admin/ashx/bg_user_login.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="bg_user_login.ashx.cs" Class="MesWebUI.admin.ashx.bg_user_login" %>
---
+++ MesDemo/MesWebUI/admin/ashx/bg_user_login.ashx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MesWebUI.admin.ashx
+{
+    /// <summary>
+    /// bg_user_login 的摘要说明
+    /// </summary>
+    public class bg_user_login : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+            string action = context.Request.Params["action"];
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
